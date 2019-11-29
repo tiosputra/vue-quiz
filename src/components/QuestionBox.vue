e4d052a132ba6c26a7df0b55423e079f1d4a86c2
karyaTio <karya.tiosaputra@gmail.com>
2019-11-29 18:41:15 +0700

    Base Layout

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <b-jumbotron lead="Bootstrap v4 Components for Vue.js 2">
+      <template v-slot:lead>
+        Some question here ?
+      </template>
+
+      <hr class="my-4" />
+
+      <p>
+        List of answers
+      </p>
+
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button variant="success" href="#">Next</b-button>
+    </b-jumbotron>
+  </div>
+</template>
